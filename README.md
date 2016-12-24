5448deb971111ce07d83ccefdbbd5cc9d7cecfe0
milenevlacerda <milenelacerda@icloud.com>
2016-12-24 18:12:52 -0200

    Estrurando diretórios e iniciando com a primeira view

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Alura-angularI
+# Alura Angular I
+
+
+### Aula 01 - Construindo o alicerce da aplicação
+--------------------------------------------------
+*Expressão angular na view*
+```
+{{ foto.titulo }}
+```
---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,15 @@
+angular.module( 'alurapic' ).controller( 'FotosController', function( $scope ) {
+
+    $scope.fotos = [
+        {
+            titulo: 'Leão',
+            url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+        },
+        {
+            titulo: 'Leão2',
+            url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+        }
+    ];
+
+
+});
